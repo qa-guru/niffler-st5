51d5f806543eec8a09c986a0bbb18bc3ebc0fbd2
Alexey <alexey.melnik@icloud.com>
2024-04-22 23:24:31 -0300

    GenerateCategory annotation added

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingTest.java
@@ -5,9 +5,12 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import guru.qa.niffler.jupiter.annotation.GenerateCategory;
 import guru.qa.niffler.jupiter.annotation.Spend;
+import guru.qa.niffler.jupiter.extension.CategoryExtension;
 import guru.qa.niffler.jupiter.extension.SpendExtension;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
+import guru.qa.niffler.page.LoginPage;
+import guru.qa.niffler.page.WelcomePage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,7 +19,7 @@ import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
-@ExtendWith(SpendExtension.class)
+@ExtendWith({SpendExtension.class, CategoryExtension.class})
 public class SpendingTest {
 
     static {
@@ -25,29 +28,31 @@ public class SpendingTest {
 
     @BeforeEach
     void doLogin() {
+        WelcomePage welcomePage = new WelcomePage();
+        LoginPage loginPage = new LoginPage();
+
         Selenide.open("http://127.0.0.1:3000/");
-        $("a[href*='redirect']").click();
-        $("input[name='username']").setValue("dima");
-        $("input[name='password']").setValue("12345");
-        $("button[type='submit']").click();
+        welcomePage.clickLoginBtn();
+        loginPage.doLogin("Aleksei", "Pass123");
     }
 
     @GenerateCategory(
             username = "Aleksei",
-            category = "Обучение"
+            category = "Досуг"
     )
     @Spend(username = "Aleksei",
             description = "QA.GURU Advanced 5",
             amount = 65000.00,
             currency = CurrencyValues.RUB,
-            category = "Обучение")
+            category = "Досуг")
     @Test
     void spendingShouldBeDeletedAfterTableAction(SpendJson spendJson) {
+       Selenide.open("http://127.0.0.1:3000/main");
         SelenideElement rowWithSpending = $(".spendings-table tbody")
                 .$$("tr")
                 .find(text(spendJson.description()));
 
-        rowWithSpending.$$("td").first().click();
+        rowWithSpending.$$("td").first().scrollTo().click();
         $(".spendings__bulk-actions button").click();
 
         $(".spendings-table tbody").$$("tr")
