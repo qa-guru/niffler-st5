93e1a3b5b070341949d8bbe2885109fbd388978f
Alexey <alexey.melnik@icloud.com>
2024-04-28 12:16:41 -0300

    methods added

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/PeoplePage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/PeoplePage.java
@@ -3,6 +3,7 @@ package guru.qa.niffler.page;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
@@ -13,5 +14,28 @@ public class PeoplePage {
 
     public SelenideElement userNameRow (String username) {
         return rows.find((text(username)));
+    }
+
+    public boolean isFriend (String username){
+        userNameRow(username).lastChild().
+                $("div").
+                shouldHave(text("You are friends"));
+        return true;
+    }
+
+    public boolean isInviteSent (String username){
+        userNameRow(username).lastChild().
+                $("div").
+                shouldHave(text("Pending invitation"));
+        return true;
+    }
+
+    public boolean isInvitationReceived(String username) {
+        userNameRow(username).lastChild().
+                $(".abstract-table__buttons div").
+                shouldHave(attribute(
+                        "data-tooltip-id",
+                        "submit-invitation"));
+        return true;
     }
 }
